39ea1a8a5b3d043177088ddbcaef14920d29cb28
William Viktorsson <william@viktorsson.com>
2021-08-26 02:11:50 +0200

    example working

--- db_example/scripts/test.js
+++
@@ -1,6 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("POST", " http://localhost:3000/authenticate", true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(JSON.stringify({
-    username: "xyz"
-}));
