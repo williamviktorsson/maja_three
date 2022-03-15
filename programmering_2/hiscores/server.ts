30d80c5f6c9cbab53b004b17a98ec1284cca8dd8
williamv <williamv@cs.umu.se>
2022-03-15 13:09:02 +0100

    example server code

--- programmering_2/hiscores/server.ts
+++ programmering_2/hiscores/server.ts
@@ -1,26 +1,79 @@
 import { JumpPlayer } from "./player";
-import { CreateLeaderboardRequest, CreateLeaderboardResponse, GetScoresRequest, GetScoresResponse } from "./requests";
+import { CreateLeaderboardRequest, CreateLeaderboardResponse, DeleteLeaderboardRequest, DeleteLeaderboardResponse, GetScoresRequest, GetScoresResponse } from "./requests";
 import { JumpScore, Score } from "./score";
 
 import * as express from 'express';
 import * as http from 'http';
+
+const MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+
 var server = express();
 const app = http.createServer(server);
 
 server.use(express.json())
 
 server.post('/leaderboard', async (req, res) => {
+
     let request: CreateLeaderboardRequest = req.body;
-
-    console.log(request);
-
     let response: CreateLeaderboardResponse = new CreateLeaderboardResponse();
 
-    response.success = true;
+    try {
+        MongoClient.connect("mongodb://localhost:27017/", async (err, client) => {
+            if (err) throw err;
+            let db = client.db("test");
 
-    return res.send(JSON.stringify(response));
+            try {
+                let result = await db.collection("leaderboards").insertOne(
+                    {
+                        "_id": request.leaderboard_id,
+                        "save_multiple_scores_per_player": request.save_multiple_scores_per_player,
+                        "scores": []
+                    })
+                response.success = result.acknowledged;
+                return res.send(JSON.stringify(response));
+            } catch (error) {
+                response.success = false;
+                return res.send(JSON.stringify(response));
+            }
+        });
+    } catch (error) {
+        response.success = false;
+        return res.send(JSON.stringify(response));
+    }
 
 });
+
+server.delete('/leaderboard', async (req, res) => {
+
+    let request: DeleteLeaderboardRequest = req.body;
+    let response: DeleteLeaderboardResponse = new DeleteLeaderboardResponse();
+
+    try {
+        MongoClient.connect("mongodb://localhost:27017/", async (err, client) => {
+            if (err) throw err;
+            let db = client.db("test");
+
+            try {
+                let result = await db.collection("leaderboards").deleteOne(
+                    {
+                        "_id": request.leaderboard_id,
+                    })
+                response.success = result.acknowledged;
+                return res.send(JSON.stringify(response));
+            } catch (error) {
+                response.success = false;
+                return res.send(JSON.stringify(response));
+            }
+        });
+    } catch (error) {
+        response.success = false;
+        return res.send(JSON.stringify(response));
+    }
+
+});
+
+
 
 server.get('/scores', async (req, res) => {
 
