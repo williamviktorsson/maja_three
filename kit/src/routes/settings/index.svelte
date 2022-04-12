ab5a864eaea8c75468eefa82f5891eab54d157d4
Magdalena Boström <magdab@cs.umu.se>
2022-04-12 10:26:54 +0200

    fixes

---
+++ kit/src/routes/settings/index.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let clicks = 0;
+	export let increase = 0;
+</script>
+
+<form action="" method="post">
+	<button class="btn btn-primary">Add!!</button>
+</form>
+
+<div class="stats shadow">
+	<div class="stat">
+		<div class="stat-title">Total clicks</div>
+		<div class="stat-value">{clicks}</div>
+		<div class="stat-desc">{increase}% more than last month</div>
+	</div>
+</div>
+
+<div class="carousel rounded-box w-96">
+	<div class="carousel-item w-1/2">
+		<img src="https://api.lorem.space/image/game?w=256&h=400&hash=8B7BCDC2" class="w-full" />
+	</div>
+	<div class="carousel-item w-1/2">
+		<img src="https://api.lorem.space/image/game?w=256&h=400&hash=500B67FB" class="w-full" />
+	</div>
+	<div class="carousel-item w-1/2">
+		<img src="https://api.lorem.space/image/game?w=256&h=400&hash=A89D0DE6" class="w-full" />
+	</div>
+	<div class="carousel-item w-1/2">
+		<img src="https://api.lorem.space/image/game?w=256&h=400&hash=225E6693" class="w-full" />
+	</div>
+	<div class="carousel-item w-1/2">
+		<img src="https://api.lorem.space/image/game?w=256&h=400&hash=9D9539E7" class="w-full" />
+	</div>
+	<div class="carousel-item w-1/2">
+		<img src="https://api.lorem.space/image/game?w=256&h=400&hash=BDC01094" class="w-full" />
+	</div>
+	<div class="carousel-item w-1/2">
+		<img src="https://api.lorem.space/image/game?w=256&h=400&hash=7F5AE56A" class="w-full" />
+	</div>
+</div>
